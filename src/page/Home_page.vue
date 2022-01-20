8b779cd6f0642c8f40397eaa0901b5b09c8ad168
gr_1_korsun_vue_project <dmitry.korsun.v@gmail.com>
2022-01-20 23:11:30 +0200

    Создано отображение блоков домашней страницы

---
+++ src/page/Home_page.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+<Home_banner/>
+    <Movie/>
+    <Details_movie/>
+  </div>
+</template>
+
+<script>
+import Home_banner from "@/components/Home_banner";
+import Movie from "@/components/Movie";
+import Details_movie from "@/page/Details_movie";
+export default {
+  name: "Home_page",
+  components: {
+    Home_banner,Movie,Details_movie
+  }
+
+}
+</script>
+
+<style>
+
+</style>
