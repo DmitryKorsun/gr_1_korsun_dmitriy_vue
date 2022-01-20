217022cec0c9e305121205123c1dae89ac2e6661
gr_1_korsun_vue_project <dmitry.korsun.v@gmail.com>
2022-01-20 23:07:18 +0200

    Мелкие изменения для отображения

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,37 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <Header />
+    <Home_page/>
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
-
+import Header from "@/components/Header";
+import Home_page from "@/page/Home_page";
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    Header,Home_page
   }
 }
 </script>
 
 <style>
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
+/*  font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
   -moz-osx-font-smoothing: grayscale;
   text-align: center;
   color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 60px;*/
+}
+* {
+  margin: 0;
+  padding: 0;
+  box-sizing: border-box;
+}
+body {
+  background-color:antiquewhite;
+  height: 100vh;
 }
 </style>
