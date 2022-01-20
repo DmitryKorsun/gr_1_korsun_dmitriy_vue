5770672d3a603bd7a9766af8a5202bceba71b8fe
gr_1_korsun_vue_project <dmitry.korsun.v@gmail.com>
2022-01-20 23:43:39 +0200

    Создан шаблон для отображения карусели и деталей к фильму

---
+++ src/components/Movie.vue
@@ -0,0 +1,131 @@
+<template>
+<div id="movies-list">
+  <h2>Новые фильмы</h2>
+  <div class="slider">
+    <carousel :per-page="6" :navigate-to="0" :mouse-drag="true" :paginationEnabled="false" :navigationEnabled="true" :navigationClickTargetSize="9">
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+    </carousel>
+  </div>
+  <h2>Рекомендуем посмотреть</h2>
+  <div class="slider">
+    <carousel :per-page="4" :navigate-to="0" :mouse-drag="true" :paginationEnabled="false" :navigationEnabled="true" :navigationClickTargetSize="9">
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+    </carousel>
+  </div>
+  <h2>Про всякое</h2>
+  <div class="slider">
+    <carousel :per-page="4" :navigate-to="0" :mouse-drag="true" :paginationEnabled="false" :navigationEnabled="true" :navigationClickTargetSize="9">
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+      <slide class="id-movie">
+        <img class="img-banner" src="../assets/movie-bannerjpg.jpg">
+      </slide>
+    </carousel>
+  </div>
+</div>
+</template>
+
+<script>
+import { Carousel, Slide } from 'vue-carousel';
+export default {
+  name: "Movie",
+  data () {
+    return {
+      paginationButtons:false
+    }
+  },
+  components: {
+    Carousel, Slide
+  }
+}
+</script>
+
+<style>
+#movies-list {
+  width: 100%;
+  height: 100%;
+  text-align: center;
+  margin: auto;
+}
+#movies-list h2{
+  color: orange;
+  font-family: Arial, Helvetica,sans-serif;
+  font-size: 22px;
+  font-weight: bold;
+  display: flex;
+  justify-content: flex-start;
+  align-items: center;
+  margin: 15px 0 10px 20px;
+  cursor: default;
+}
+.slider {
+  width: 90%;
+  height: 100%;
+  text-align: center;
+  margin: auto;
+}
+.id-movie {
+  margin-top:23px;
+  padding-left: 20px;
+  flex: 1;
+}
+.img-banner {
+  border-radius: 15px;
+  transition: 0.5s;
+  height: 300px;
+  margin-bottom: 20px;
+  width: 200px;
+}
+.img-banner:hover {
+  transform: scale(1.1);
+  cursor: pointer;
+}
+.VueCarousel-navigation-button {
+  color: orange !important;
+  outline: none !important;
+}
+</style>
---
+++ src/page/Details_movie.vue
@@ -0,0 +1,146 @@
+<template>
+  <div id="details-movie">
+    <div id="container-details">
+      <div id="movie-info-watch">
+
+      </div>
+      <div id="description-movie">
+        <div id="title-movie">
+          Охотники за приведениями
+        </div>
+        <div id="plot-movie">
+          В городке Саммервилл где-то в Оклахоме творятся странные дела — конспирологические теории летают в воздухе,
+          и даже школьные учителя всерьез говорят о странной сейсмической активности. Но всё это не выдумки скучающих
+          провинциалов, а странное наследие местной легенды — «грязного фермера», который погиб, пытаясь оградить
+          родные края от нечисти.
+        </div>
+        <button id="add-favorites-movie" @click="add_to_favorites_list">Добавить в избранное</button>
+        <div id="full-details-movie">
+          <p>
+            Время просмотра
+            <span>60 минут</span>
+          </p>
+          <p>
+            Актеры
+            <span>Кэрри Кун, Пол Радд, Финн Вулфхард, МакКенна Грейс, Логан Ким, Селеста О’Коннор, Билл Мюррей, Дэн Эйкройд, Эрни Хадсон, Энни Поттс</span>
+          </p>
+          <p>
+            Режиссёр
+            <span>Джейсон Райтман</span>
+          </p>
+          <p>
+            Жанр
+            <span>Фантастика, Комедии, Боевики</span>
+          </p>
+          <p>
+            Аудиодорожки
+            <span>Русский</span>
+          </p>
+          <p>
+            Награды
+            <span>Высшая награда за труд бла бла бла</span>
+          </p>
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: "Details_movie"
+}
+</script>
+
+<style>
+#details-movie {
+  background: gray;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+
+#container-details {
+  width: 100%;
+  height: 100%;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+}
+
+#add-favorites-movie {
+  display: flex;
+  text-align: center;
+  justify-content: center;
+  align-items: center;
+  text-decoration: none;
+  background: orange;
+  border-radius: 25px;
+  border: none;
+  height: 40px;
+  width: 250px;
+  font-size: 16px;
+  font-weight: bold;
+  color: white;
+  margin-bottom: 30px;
+  transition: transform 0.5s;
+}
+
+#add-favorites-movie:hover {
+  transform: scale(1.2);
+  cursor: pointer;
+  color: black;
+}
+
+#description-movie {
+  display: flex;
+  flex-direction: column;
+  justify-content: space-around;
+  align-items: center;
+  width: 45%;
+  color: orange;
+  font-family: "Lucida Sans","Lucida Sans",Geneva, Verdana,sans-serif;
+  text-align: justify;
+}
+
+#title-movie {
+  width: 58%;
+  color: orange;
+  text-shadow: black 0.1em 0.1em 0.2em;
+  font-size: 35px;
+  text-align: center;
+  font-family: Arial,Helvetica,sans-serif;
+}
+
+#plot-movie {
+color: #ffB100;
+  font-family: Arial,sans-serif;
+  font-size: 14px;
+  text-align: justify;
+  text-shadow: black 0.1em 0.1em 0.2em;
+  margin: 20px 0;
+  flex-direction: column;
+  width: 60%;
+}
+#full-details-movie {
+  display: grid;
+  grid-template-columns: repeat(3,1fr);
+  grid-gap: 30px;
+}
+#full-details-movie p{
+  border: 2px solid black;
+  border-radius: 20px;
+  display: flex;
+  flex-direction: column;
+  text-align: center;
+  color: orange;
+  text-shadow: black 0.1em 0.1em 0.2em;
+  font-weight: bold;
+  font-size: 18px;
+  margin-bottom: 15px;
+}
+#full-details-movie span {
+  color: black;
+}
+</style>
