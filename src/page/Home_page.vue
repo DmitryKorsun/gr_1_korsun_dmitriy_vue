e074caa038ab644d8836148dcfd59c70c7477d07
gr_1_korsun_vue_project <dmitry.korsun.v@gmail.com>
2022-01-21 10:26:31 +0200

    Добавлен блок баннера на главную страницу

--- src/page/Home_page.vue
+++ src/page/Home_page.vue
@@ -1,19 +1,19 @@
 <template>
   <div>
-<Home_banner/>
+    <Banner/>
     <Movie/>
     <Details_movie/>
   </div>
 </template>
 
 <script>
-import Home_banner from "@/components/Home_banner";
+import Banner from "@/components/Banner";
 import Movie from "@/components/Movie";
 import Details_movie from "@/page/Details_movie";
 export default {
   name: "Home_page",
   components: {
-    Home_banner,Movie,Details_movie
+    Banner,Movie,Details_movie
   }
 
 }
